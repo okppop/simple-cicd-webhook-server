848231210f929f770c95dc77580d909c986c6c60
okppop <okppop@protonmail.com>
2024-12-12 01:43:06 +0800

    add github event detect

--- main.go
+++ main.go
@@ -11,12 +11,13 @@ func main() {
 	http.HandleFunc("/webhook", webhookHandler)
 	err := http.ListenAndServe(":8080", nil)
 	if err != nil {
-		log.Fatal("exit: server init: ", err)
+		log.Fatal("Fatal: http.ListenAndServe: ", err)
 	}
 }
 
 func webhookHandler(w http.ResponseWriter, r *http.Request) {
 	// no need to print the payload to stdout
+
 	// body, err := io.ReadAll(r.Body)
 	// if err != nil {
 	// 	log.Print("Error: webhookHandler: io.ReadAll: ", err)
@@ -26,7 +27,31 @@ func webhookHandler(w http.ResponseWriter, r *http.Request) {
 	// log.Print("Info: webhookHandler: new request, payload: ")
 	// fmt.Println(string(body))
 
-	log.Print("Info: webhookHandler: new request, calling action")
+	log.Print("Info: /webhook: receive a new request")
+
+	if r.Method != "POST" {
+		log.Print("Warn: /webhook: http method isn't 'POST': drop")
+		return
+	}
+
+	var httpHeaderXGitHubEvent string = r.Header.Get("X-GitHub-Event")
+
+	if httpHeaderXGitHubEvent == "" {
+		log.Print("Warn: /webhook: http header 'X-GitHub-Event' is empty: drop")
+		return
+	}
+
+	if httpHeaderXGitHubEvent == "ping" {
+		log.Print("Info: /webhook: http header 'X-GitHub-Event' is 'ping': ignore")
+		return
+	}
+
+	if httpHeaderXGitHubEvent != "push" {
+		log.Printf("Info: /webhook: http 'X-GitHub-Event' is a undefinded event '%s': ignore", httpHeaderXGitHubEvent)
+		return
+	}
+
+	log.Print("Info: /webhook: detect 'push' event, calling action")
 
 	err := action()
 	if err != nil {
@@ -40,7 +65,6 @@ func webhookHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func action() error {
-	// change this to any action you want
 	cmd := exec.Command("/usr/bin/ls", "-l")
 	cmd.Stderr = os.Stderr
 	cmd.Stdout = os.Stdout
@@ -49,5 +73,6 @@ func action() error {
 	if err != nil {
 		return err
 	}
+
 	return nil
 }
